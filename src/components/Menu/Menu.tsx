f86cb086ad2f6f7c7d2859a6e5ef8fbc8db185ae
alysonbarreto02 <alysonbarretito2123@gmail.com>
2022-05-17 16:59:49 -0300

    bug resolvido

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,4 +1,4 @@
-import { Topo } from "./styles";
+import { Name, NavBar, Topo } from "./styles";
 
 
 export function Menu() {
@@ -6,17 +6,19 @@ export function Menu() {
 
         <div>
             <Topo>
-                <div className="name">
-                    <h1><span> Alyson</span> Kevin</h1>
-                </div>
-                <nav className="navBar">
-                    <ul>
-                        <li><a href="">Home</a></li>
-                        <li><a href="">About me</a></li>
-                        <li><a href="">Contact</a></li>
-                    </ul>
-                </nav>
+                <Name>
+                    <h1><span>Alyson</span> Kevin</h1>
+                </Name>
+                <NavBar>
+                        <ul>
+                            <li><a href="">Home</a></li>
+                            <li><a href="">About me</a></li>
+                            <li><a href="">Skills</a></li>
+                            <li><a href="">Contact</a></li>
+                        </ul>
+                </NavBar>
             </Topo>
+            <hr />
         </div>
 
     )
--- src/components/Menu/styles.ts
+++ src/components/Menu/styles.ts
@@ -2,22 +2,45 @@ import styled from "styled-components";
 
 export const Topo = styled.header`
     display: flex;
-    height: 30px;
+    height: 5rem;
     width: 100%;
 
-    .name{
-        span{
-            color: #C8DE4E;
-        }
-        color: white;
-        font-size: 25px;
-    }
 
-    .navBar{
-        justify-content: space-between;
+
+;
+`
+export const Name = styled.div`
+    height: 5rem;
+    line-height: 5rem;
+    margin-left: 4rem;
+
+    span{
+        color: #C8DE4E;
+    }
+    h1{
+        color: white;
+        font-size: 40px;
     }
 `
 
-export const Name = styled.div`
+export const NavBar = styled.nav`
+    ul{
+        font-size: 24px;
 
+        display: flex;
+        margin-left: 60rem;
+        height: 5rem;
+        line-height: 5rem;
+
+        color: gray;
+    }
+    
+    li{
+        margin: 0 30px;
+    }
+    a{
+        color: white;
+        font-weight: 500;
+        text-decoration: none;
+    }
 `
